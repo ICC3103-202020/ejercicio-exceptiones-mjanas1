1c14e053d8fd6ef5503d86ed19a09a5501184c54
mjanas1 <mjanas@miuandes.cl>
2020-10-06 22:29:08 -0400

    Ejercicio exception

---
+++ ejercico_excepciones/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercico_excepciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int GetAltura()
+            {
+             int h = Convert.ToInt32(Console.ReadLine());
+                if (h < 150)
+                {
+                    throw new bajaAltura("Para ingresar a la montaña rusa no se acepta esta altura");
+                }
+                return h;
+            }
+            int a;
+            try { 
+            Console.Write("Selecciona tu altura en cm ");
+            a = GetAltura();
+            Console.WriteLine("Puedes ingresar a la montaña rusa");
+            Console.ReadLine();
+            }
+            catch(bajaAltura e)
+            {
+                Console.WriteLine(e.Message);
+                Console.ReadLine();
+            }
+        }
+
+    }
+    public class bajaAltura : Exception
+    {
+        private readonly int altura;
+
+        public bajaAltura(string message) : base(message)
+        {
+
+        }
+    }
+}
